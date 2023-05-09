086191fa16379cfbb7322647285b9e83ec486884
Progress <ezeamakachisom2002@gmail.com>
2023-05-09 14:24:56 +0100

    Add patients and medical history schema

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "patients" (
+  "id" INT,
+  "name" VARCHAR(60),
+  "date_of_birth" DATE,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "medical_histories" (
+  "id" INT,
+  "admitted" TIMESTAMP,
+  "patient_id" INT,
+  "status" VARCHAR(60),
+  PRIMARY KEY ("id"),
+  CONSTRAINT "FK_medical_histories.patient_id"
+    FOREIGN KEY ("patient_id")
+      REFERENCES "patients"("id")
+);
